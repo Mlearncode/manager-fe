3cac67a6e32bc6e3b867d1ef9a1f9bb686909127
mccode <2447505733@qq.com>
2023-05-06 15:36:43 +0800

    feat: request封装

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import router from './router'
 import config from './config'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
-import axios from 'axios'
+import request from './utils/request'
 
+App.config.globalProperties.$request = request
 
 createApp(App).use(router).use(ElementPlus).mount('#app')
